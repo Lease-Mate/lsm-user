f22c47d74b856b133213f69218470a672f3c473c
Filip Pamuła <filippamula@onet.pl>
2024-11-05 17:51:40 +0100

    fixed register and login request model

--- src/main/java/com/lsm/ws/user/context/auth/dto/LoginRequest.java
+++ src/main/java/com/lsm/ws/user/context/auth/dto/LoginRequest.java
@@ -2,8 +2,11 @@ package com.lsm.ws.user.context.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public record LoginRequest(@Email
+                           @NotEmpty
                            String email,
                            @NotEmpty
                            String password) {
--- src/main/java/com/lsm/ws/user/context/auth/dto/RegisterRequest.java
+++ src/main/java/com/lsm/ws/user/context/auth/dto/RegisterRequest.java
@@ -1,15 +1,15 @@
 package com.lsm.ws.user.context.auth.dto;
 
-import jakarta.validation.constraints.NotNull;
-import org.springframework.validation.annotation.Validated;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Date;
 
 @Validated
 public record RegisterRequest(@Email
+                              @NotEmpty
                               String email,
 
                               @NotEmpty
