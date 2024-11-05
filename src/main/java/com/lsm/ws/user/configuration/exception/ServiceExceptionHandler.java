e7bfbe7aa9e36f3664110f6685cacc85378f6924
Filip Pamuła <filippamula@onet.pl>
2024-11-05 17:03:50 +0100

    fixed field validator exception handling

--- src/main/java/com/lsm/ws/user/configuration/exception/ServiceExceptionHandler.java
+++ src/main/java/com/lsm/ws/user/configuration/exception/ServiceExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.lsm.ws.user.configuration.exception;
 
 import com.lsm.ws.user.configuration.exception.dto.ErrorCodeResponse;
+import com.lsm.ws.user.configuration.exception.dto.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -23,6 +26,17 @@ public class ServiceExceptionHandler {
         LOGGER.info("HTTP 400 - Invalid request {} CODE: {} reason: {}",
                 webRequest.getDescription(false), exception.code(), exception.getMessage());
         var responseBody = new ErrorCodeResponse(exception.code(), exception.getMessage());
+        return new ResponseEntity<>(responseBody, BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException exception) {
+        var cause = exception.getBindingResult().getAllErrors().get(0);
+        var message = cause.getDefaultMessage();
+        if (cause instanceof FieldError) {
+            message = (((FieldError) cause).getField()) + " " + message;
+        }
+        var responseBody = new ErrorResponse(message);
         return new ResponseEntity<>(responseBody, BAD_REQUEST);
     }
 }
