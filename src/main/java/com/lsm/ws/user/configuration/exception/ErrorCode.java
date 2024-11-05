a7c75c257ce9dd30ae58ae4415a452c05189618f
Filip Pamuła <filippamula@onet.pl>
2024-11-05 17:17:01 +0100

    changed error responses language

--- src/main/java/com/lsm/ws/user/configuration/exception/ErrorCode.java
+++ src/main/java/com/lsm/ws/user/configuration/exception/ErrorCode.java
@@ -2,9 +2,9 @@ package com.lsm.ws.user.configuration.exception;
 
 public enum ErrorCode {
 
-    USER_ALREADY_EXIST("001", "User with username: {} already exist"),
-    INVALID_CREDENTIALS("002", "Invalid credentials"),
-    TRY_AGAIN_LATER("003", "Try again later");
+    USER_ALREADY_EXIST("001", "Użytkownik z adresem email {} już istnieje"),
+    INVALID_CREDENTIALS("002", "Niepoprawny email lub hasło"),
+    TRY_AGAIN_LATER("003", "Spróbuj ponownie później");
 
     private final String code;
     private final String message;
